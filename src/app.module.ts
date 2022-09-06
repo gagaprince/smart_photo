5bb1d7b03d041772219d2f4359ceb75a1d0f388f
gagaprince <wangzidongprince@163.com.com>
2022-09-06 22:16:41 +0800

    feat: 去除apimodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SpiderphotoModule } from './modules/spiderphoto/spiderphoto.module';
-import { ApiModule } from './modules/api/api.module';
+// import { SpiderphotoModule } from './modules/spiderphoto/spiderphoto.module';
+// import { ApiModule } from './modules/api/api.module';
 import { SmallvideoService } from './modules/smallvideo/smallvideo.service';
 import { SmallvideoController } from './modules/smallvideo/smallvideo.controller';
 import { SmallvideoModule } from './modules/smallvideo/smallvideo.module';
 
 @Module({
-  imports: [SpiderphotoModule, ApiModule, SmallvideoModule],
+  // imports: [SpiderphotoModule, ApiModule, SmallvideoModule],
+  imports: [SmallvideoModule],
   controllers: [AppController, SmallvideoController],
   providers: [AppService, SmallvideoService],
 })
